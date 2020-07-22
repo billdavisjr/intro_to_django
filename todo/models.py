a18f35e7489853d47e00ca3c6f60c77ba156a899
Bill Davis <bill.davis@gmail.com>
2020-07-22 05:21:20 +0000

    Added code to display Items model on todo page

--- todo/models.py
+++ todo/models.py
@@ -4,5 +4,8 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharFIeld(max_length=50, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
