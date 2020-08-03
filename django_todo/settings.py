687d3fe75ba96008162bfc6d031cc65fb8dcdca6
Bill Davis <bill.davis@gmail.com>
2020-08-03 00:54:29 +0000

    Added database setting, updated install notes and requirements, prepare to commit to Heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,13 +75,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+    'default':  dj_database_url.parse('postgres://kgacudcuwskfya:0ba68f1fb7596c117ce04342466b9bdef5efb487fbf087131fb5ac93e73e9e11@ec2-34-195-115-225.compute-1.amazonaws.com:5432/d83uem98b381dq')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- installs.sh
+++ installs.sh
@@ -8,7 +8,13 @@ pip3 install coverage
 pip3 install psycopg2-binary
 
 # Gunicorn / Green Unicorn - production server package
-pip 3 install gunicorn
+pip3 install gunicorn
+
+# Database Connection URL pacakge
+pip3 install dj-database-url
+
+
+
 
 # Create requirements.txt file
 pip3 freeze --local >requirements.txt
